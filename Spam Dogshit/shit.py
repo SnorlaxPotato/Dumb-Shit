2574f9017e78fd7f5c37503e684d5cde381257f5
SnorlaxPotato <75638386+SnorlaxPotato@users.noreply.github.com>
2021-07-03 16:42:44 -0400

    Update shit.py

--- Spam Dogshit/shit.py
+++ Spam Dogshit/shit.py
@@ -1,5 +1,6 @@
 from requests import get
 from time import sleep as wait
+normal_dog = get("https://pastebin.com/raw/KiNUge03").text
 dogshit = get("https://pastebin.com/raw/KmrhMTqT").text
 while True:
     wait(1)
