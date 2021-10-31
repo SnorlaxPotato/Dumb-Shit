d1a6de72e93797d629a4fdd7818ca275155cfa50
SnorlaxPotato <75638386+SnorlaxPotato@users.noreply.github.com>
2021-10-31 17:56:25 -0400

    Update shit.py

--- Spam Dogshit/shit.py
+++ Spam Dogshit/shit.py
@@ -1,8 +1,6 @@
 from requests import get
 from time import sleep as wait
 shit = [get("https://pastebin.com/raw/KiNUge03").text, get("https://pastebin.com/raw/KmrhMTqT").text]
-i = 0
 while True:
-    wait(1)
-    print(shit[i%2])
-    i += 1
+    print(shit[0])
+    print(shit[1])
